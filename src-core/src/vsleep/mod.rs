b74b77f460aa03345fa109b23b9d1e6bf4df41b4
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-21 12:00:37 +0800

    feat: export VSleep session file metadata

--- src-core/src/vsleep/mod.rs
+++ src-core/src/vsleep/mod.rs
@@ -3,5 +3,5 @@ mod journal;
 
 pub use event::{EventConfidence, EventKind, EventSource, SessionEvent};
 pub use journal::{
-    JournalError, SessionJournal, SessionJournalStore, DEFAULT_MAX_SESSION_FILES,
+    JournalError, SessionFileInfo, SessionJournal, SessionJournalStore, DEFAULT_MAX_SESSION_FILES,
 };
