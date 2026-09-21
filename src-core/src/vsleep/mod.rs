34d896f794ae8455075ce4d3431780dffc35032e
Slava Chan <49863417+UyNewNas@users.noreply.github.com>
2026-09-21 13:01:53 +0800

    feat(vsleep): export journal runtime owner

--- src-core/src/vsleep/mod.rs
+++ src-core/src/vsleep/mod.rs
@@ -1,7 +1,9 @@
 mod event;
 mod journal;
+mod runtime;
 
 pub use event::{EventConfidence, EventKind, EventSource, SessionEvent};
 pub use journal::{
     JournalError, SessionFileInfo, SessionJournal, SessionJournalStore, DEFAULT_MAX_SESSION_FILES,
 };
+pub use runtime::{RuntimeError, SessionJournalRuntime};
